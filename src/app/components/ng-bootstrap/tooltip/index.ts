127a30840fd98cda80eb3ca5653329977e291e03
Matthew Wright <1815200+matthewguywright@users.noreply.github.com>
2019-10-06 19:25:51 -0500

    Add files via upload

---
+++ src/app/components/ng-bootstrap/tooltip/index.ts
@@ -0,0 +1,19 @@
+import { NgbdTooltipBasic } from './basic/tooltip-basic';
+import { NgbdTooltipContainer } from './container/tooltip-container';
+import { NgbdTooltipTplcontent } from './tplcontent/tooltip-tplcontent';
+import { NgbdTooltipTplwithcontext } from './tplwithcontext/tooltip-tplwithcontext';
+import { NgbdTooltipTriggers } from './triggers/tooltip-triggers';
+import { NgbdTooltipConfig } from './config/tooltip-config';
+import { NgbdTooltipAutoclose } from './autoclose/tooltip-autoclose';
+import { NgbdTooltipCustomclass } from './customclass/tooltip-customclass';
+
+export const TOOLTIP_DIRECTIVES = [
+  NgbdTooltipBasic,
+  NgbdTooltipContainer,
+  NgbdTooltipTplcontent,
+  NgbdTooltipTriggers,
+  NgbdTooltipConfig,
+  NgbdTooltipTplwithcontext,
+  NgbdTooltipAutoclose,
+  NgbdTooltipCustomclass
+];
